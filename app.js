45bf4b8347cfde89e6d79450588db272669061d8
kdpark <wert1229@gmail.com>
2020-11-17 00:30:11 +0900

    기능변경: 리사이즈 기능 수정

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-var test = require('./image');
-app.use('/image', test);
+const imageRoute = require('./image');
+app.use("/image", imageRoute);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
